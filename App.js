465cf4eb23bb33ce366bc5f8f5147d771e0ca751
users/tonderai/679 <Tonderai@lipapayments.com>
2022-03-31 16:37:26 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react'
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Ayo whats up')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button title="Change the text" onPress={() => setOutputText('The text has changed')}/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,3 +21,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+ 
